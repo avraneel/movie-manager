0908bdd2edb287e83bd6a65f07a6b4a44706b089
Avraneel Pal <avraneelpal22@gmail.com>
2026-07-05 23:01:05 +0530

    Add values to db

--- populatedb.js
+++ populatedb.js
@@ -1,24 +1,82 @@
 // Run only one time to create database
+const { Client } = require("pg");
 
 const INIT_DB_QUERY = `
 DROP DATABASE movie-manager;
 CREATE DATABASE movie_manager;
 
 CREATE TABLE IF NOT EXISTS directors (
-    director_id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name varchar(255) NOT NULL,
+    id      integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name    varchar(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS movies (
-    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    movie_name varchar(255) NOT NULL,
-    director_id integer REFERENCES directors (director_id),
-    release_date timestamp,
-    run_time interval,
+    id        integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name      varchar(255) NOT NULL,
+    director_id     integer REFERENCES directors,
+    release_date    date NOT NULL,
+    runtime         interval HOUR TO MINUTE NOT NULL,
 );
 
 CREATE TABLE IF NOT EXISTS actors (
-    actor_id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name varchar(255) NOT NULL,
+    id      integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name    varchar(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS castings (
+    actor_id        integer REFERENCES actors,
+    movie_id        integer REFERENCES movies,
+    character       varchar(255) NOT NULL,
+    PRIMARY KEY (actor_id, movie_id)
 );
 `;
+
+const INSERT_VALUES_QUERY = `
+INSERT INTO directors (name) VALUES
+    ('Christopher Nolan'),
+    ('Steven Spielberg'),
+    ('David Fincher'),
+    ('Ridley Scott');
+
+INSERT INTO movies (name, director_id, release_date, runtime) VALUES
+    ('Interstellar', (SELECT id FROM directors WHERE directors.name = 'Christopher Nolan'), '2014-10-26', '2 hours 49 minutes'),
+    ('Jurassic Park', (SELECT id FROM directors WHERE directors.name = 'Steven Spielberg'), '1993-06-09', '2 hours 7 minutes'),
+    ('The Social Network', (SELECT id FROM directors WHERE directors.name = 'David Fincher'), '2010-09-24', '2 hours'),
+    ('The Martian', (SELECT id FROM directors WHERE directors.name = 'Ridley Scott'), '2011-09-11', '2 hours 24 minutes');
+
+INSERT INTO actors (name) VALUES
+    ('Matthew McConaughey'),
+    ('Anne Hathway'),
+    ('Matt Damon'),
+    ('Sam Neill'),
+    ('Jeff Goldblum'),
+    ('Richard Attenborough'),
+    ('Jesse Eisenberg'),
+    ('Andrew Garfield');
+
+INSERT INTO castings (actor_id, movie_id, character) VALUES
+    (1, 1, 'Cooper'),
+    (2, 1, 'Brand'),
+    (3, 1, 'Mann'),
+    (9, 2, 'Grant'),
+    (5, 2, 'Malcolm'),
+    (6, 2, 'Hammond'),
+    (7, 3, 'Mark Zuckerberg'),
+    (8, 3, 'Eduardo Saverin'),
+    (3, 4, 'Mark Watney');
+`;
+
+process.loadEnvFile(".env");
+
+async function initDb() {
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+  });
+
+  await client.connect();
+  await client.query(INIT_DB_QUERY);
+  await client.query(INSERT_VALUES_QUERY);
+  await client.end();
+}
+
+initDb();
